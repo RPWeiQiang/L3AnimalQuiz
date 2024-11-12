4328ab85777c5fa472a7ad700eedb0aa176a268f
RPWeiQiang <23013990@myrp.edu.sg>
2024-11-12 13:46:40 +0800

    Animal quiz

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert, Image, ScrollView } from 'react-native';
+import { View, Text, TextInput, Button, Alert, Image, ScrollView,StyleSheet } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import Icon from "react-native-vector-icons/FontAwesome6";
 
@@ -14,12 +14,70 @@ const InputBox = ({ label, onChangeText }) => {
         </View>
     );
 };
+const styles = StyleSheet.create({
+  container:{
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#f0f8ff',
+      padding:10,
+      paddingTop:20,
+      borderRadius:10,
+      borderWidth:1,
+      borderColor:'#c4d4e2',
+      marginTop:10,
+  },
+  title:{
+      fontWeight:'bold',
+      verticalAlign:'middle',
+      textAlign:'center',
+      backgroundColor:'#5d8aa8',
+      padding:5,
+      color:'whitesmoke',
+      fontSize:20,
+  },
+  opt:{
+      borderRadius:5,
+      borderWidth:1,
+      borderColor:'#a1caf1',
+      marginTop:10,
+      backgroundColor:'white',
+  },
+  image:{
+      borderRadius:5,
+      borderWidth:5,
+      borderColor:'#dff0ff',
 
+  },
+  icon:{
+      flex:1,
+      justifyContent:'center',
+      backgroundColor:'white',
+      borderWidth:1,
+      borderRadius:50,
+      marginBottom:20,
+      borderColor:'#c4d4e2',
+      color:'white',
+      fontWeight:900,
+      alignSelf:'center',
+  },
+  border:{
+      marginBottom:20,
+      borderBottomWidth:5,
+      borderStyle:'dashed',
+      borderColor:'#5d8aa8',
+  },
+
+
+
+
+})
 const Quiz = ({ img, opt1, opt2, opt3, onSelectAnswer }) => {
     return (
-        <View style={{ marginBottom: 20 }}>
-            <Image source={img} style={{ width: 400, height: 300 }} />
-            <Text style={{ marginTop: 10 }}>What animal is this?</Text>
+        <View style={[styles.container, { marginBottom: 20 }]}>
+            <Icon name="circle" size={40} style={styles.icon} />
+            <Image source={img} style={[styles.image,{ width: 350, height: 250 }]} />
+            <Text style={[styles.title,{ marginTop: 10 }]}>What animal is this?</Text>
+            <View style={styles.opt}>
             <RNPickerSelect
                 onValueChange={(value) => onSelectAnswer(value)}
                 items={[
@@ -28,6 +86,7 @@ const Quiz = ({ img, opt1, opt2, opt3, onSelectAnswer }) => {
                     { label: opt3, value: 3 },
                 ]}
             />
+            </View>
         </View>
     );
 };
@@ -59,14 +118,17 @@ const MyApp = () => {
     };
 
     return (
-        <View style={{ padding: 20, paddingTop: 50, paddingBottom: 100}}>
-            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                <Icon name="paw" size={30} color="red" />
-                <Text style={{ color: 'black', fontSize: 24, fontWeight: 'bold'}}>Animal Quiz</Text>
-                <Icon name="paw" size={30} color="red" />
+        <View style={{ padding: 20, paddingTop: 50, paddingBottom: 150}}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center',marginBottom:10 }}>
+                <Icon name="paw" size={30} color="#5d8aa8" />
+                <Text style={{ color: 'black', fontSize: 30, fontWeight: 'bold',}}>Animal Quiz</Text>
+                <Icon name="paw" size={30} color="#5d8aa8"  />
             </View>
-            <InputBox label="Username"  onChangeText={(text) => setUsername(text)} />
+            <View style={styles.border}>
+            <InputBox style={styles.username} label="Username"  onChangeText={(text) => setUsername(text)} />
+            </View>
             <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
+                <View style={styles.border}>
                 <Quiz
                     img={require('./img/elephant.jpg')}
                     opt1="Elephant"
@@ -91,8 +153,10 @@ const MyApp = () => {
                     correctValue={3}
                     onSelectAnswer={(value) => AnswerSelect(value, 3)}
                 />
+                </View>
                 <Button title="Submit Answers" onPress={Submit} />
             </ScrollView>
+
         </View>
     );
 };
